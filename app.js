4b4cf93c26325e8a20f893424de20c1623d2bc1c
Salieu Sarr <95061054+Ssrr17@users.noreply.github.com>
2022-03-02 16:01:45 -0500

    multiple projects

--- app.js
+++ app.js
@@ -10,13 +10,86 @@ const inquirer = require('inquirer');
 
 //   console.log('Portfolio complete! Check out index.html to see the output!');
 // });
+const promptUser = () => {
+  return inquirer.prompt([
 
-inquirer
-  .prompt([
     {
       type: 'input',
       name: 'name',
       message: 'What is your name?'
+    },
+    {
+      type: 'input',
+      name: 'Github',
+      message: 'Enter your Github username'
+    },
+    {
+      type: 'input',
+      name: 'About',
+      message: 'Provide so information about yourself'
+    }
+
+  ])
+}
+const promptProject = portfolioData => {
+ 
+  // If there's no 'projects' array property, create one
+  if (!portfolioData.projects) {
+    portfolioData.projects = [];
+  }
+  console.log(
+    `==================
+    Add a new project
+    ==================`
+  );
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'Project Name',
+      message: 'Enter your project name'
+    },
+    {
+      type: 'input',
+      name: 'descriptions',
+      message: 'Enter a description of your project (Required)'
+    },
+    {
+      type: 'checkbox',
+      name: 'languages',
+      message: 'What did you build your propject with? (Check all that apply)',
+      choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Boottrap', 'Node']
+    },
+    {
+      type: 'input',
+      name: 'link',
+      message: 'Enter the GitHub link to your project (Required)'
+    },
+    {
+      type: 'confirm',
+      name: 'feature',
+      message: 'Would you like to feature this project?',
+      default: false
+    },
+    {
+      type: 'confirm',
+      name: 'confirmAddProject',
+      message: 'Would you like to enter another project?',
+      default: false
     }
   ])
-  .then(answers => console.log(answers));
+  .then(projectData => {
+    portfolioData.projects.push(projectData);
+    if (projectData.confirmAddProject) {
+      return promptProject(portfolioData);
+    } else {
+      return portfolioData;
+    }
+  });
+}
+//calls promptUser - then gets promise - then what to do with the promise that was returned
+
+promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+   console.log(portfolioData);
+  });
